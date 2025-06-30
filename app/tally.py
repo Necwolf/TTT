df335c9e750baf348fee44bf3f47e41f248f62e2
NecWolf <vencwtf3365@gmaiil.com>
2025-06-30 17:22:45 +0300

    18 flat_data + TG_clean_msg

--- app/tally.py
+++ app/tally.py
@@ -1,6 +1,6 @@
 from fastapi import Request
 from app.bot import bot
-from app.utils import flatten_fields
+from app.utils import flatten_fields, format_travel_message
 
 iduser_test = "188539449"
 
@@ -22,8 +22,7 @@ async def tally_webhook_trip(request: Request):
     flat_data = flatten_fields(raw_data)
     print("CLEANED:", flat_data)
 
-    # name = flat_data.get('👤CONTACT PERSON "Товари":')
-    # print(name)
-
-    await bot.send_message(chat_id=iduser_test, text="info delivered")
+    message = format_travel_message(flat_data)
+    await bot.send_message(chat_id=iduser_test, text=message, parse_mode="HTML")
+    # await bot.send_message(chat_id=iduser_test, text="info delivered")
     return {"status": "ok"}
