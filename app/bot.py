373a4e4408bce896a6a5cf2c4c53a7ec847c257e
NecWolf <vencwtf3365@gmaiil.com>
2025-06-30 16:15:24 +0300

    16 aiogram test

--- app/bot.py
+++ app/bot.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 TOKEN = "1402083780:AAFI3e8sgo6C1VL71LOi0Wf3MzLXJex6YUY"
-bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
